029c87acc774e6d931d5a1c1d775c337bcf6150e
ironvenjix <88736855+ironvenjix@users.noreply.github.com>
2021-08-11 12:05:03 +0530

    Add files via upload

---
+++ area.py
@@ -0,0 +1,4 @@
+import math
+radius = float(input("enter the radius of the circle : "))
+area = (radius**2) * (math.pi)
+print("the are of the cirle is : ",area)
---
+++ ext.py
@@ -0,0 +1,4 @@
+name = input("enter the full file name : ")
+ext = name.split(".")
+
+print ("Extension of the file is : " + ext[1])
