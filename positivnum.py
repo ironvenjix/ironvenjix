dbcd7c967f7c5e577fb0885cf084987450d64efa
ironvenjix <88736855+ironvenjix@users.noreply.github.com>
2021-08-20 22:46:44 +0530

    Delete positivnum.py

--- positivnum.py
+++
@@ -1,5 +0,0 @@
-ll = int(input("enter the lower limit of the range of numbers you want to print : "))
-ul = int(input("enter the upper limit of the range of numbers you want to print : "))
-for i in range(ll,ul):
-    print(i)
-    i +=1
